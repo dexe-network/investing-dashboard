85ad6250fb19243ccc791f8823d2e5417b583259
Arti <z.artemznahar@gmail.com>
2022-03-28 18:20:43 +0300

    feat: created a header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,66 @@
+import { StyledBar, TitleMenu, TabsMenu } from "components/TopMembersBar/styled"
+import { useState } from "react"
+import { usePoolsFilters } from "state/pools/hooks"
+import { Tabs, Tab, Title, titleVariants } from "./styled"
+
+interface ITab {
+  title: string
+  source: string
+  amount: number
+}
+
+interface Props {
+  title: string
+  LeftComponent: any
+  RightComponent: any
+  tabs: ITab[]
+  isTitleHidden?: boolean
+}
+
+const Header = ({
+  LeftComponent,
+  RightComponent,
+  title,
+  tabs,
+  isTitleHidden,
+}: Props) => {
+  const [filters, dispatchFilter] = usePoolsFilters()
+  return (
+    <StyledBar
+      initial={{ y: -102 }}
+      animate={{ y: 0 }}
+      exit={{ y: -102 }}
+      transition={{ duration: 0.4, ease: [0.29, 0.98, 0.29, 1] }}
+    >
+      <TitleMenu>
+        <LeftComponent />
+        <Title
+          initial="visible"
+          animate={isTitleHidden ? "hidden" : "visible"}
+          variants={titleVariants}
+          transition={{ duration: 0.1, ease: [0.29, 0.98, 0.29, 1] }}
+        >
+          {title}
+        </Title>
+        <RightComponent />
+      </TitleMenu>
+      <TabsMenu>
+        <Tabs>
+          {tabs.map((tab: ITab) => {
+            return (
+              <Tab
+                key={tab.title}
+                active={filters.listType === tab.title}
+                onClick={() => dispatchFilter("listType", tab.title)}
+              >
+                {tab.title}
+              </Tab>
+            )
+          })}
+        </Tabs>
+      </TabsMenu>
+    </StyledBar>
+  )
+}
+
+export default Header
---
+++ src/components/Header/styled.tsx
@@ -0,0 +1,103 @@
+import { Flex, BaseButton } from "theme"
+import styled from "styled-components"
+
+export const ClickableArea = styled(BaseButton)`
+  padding: 10px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 24px;
+  min-width: 24px;
+`
+
+export const Tabs = styled(Flex)`
+  width: 100%;
+  padding: 0 10px;
+  position: relative;
+  justify-content: space-around;
+  height: 34px;
+
+  &:before {
+    position: absolute;
+    top: 0;
+    right: 0;
+    left: 0;
+    margin: auto;
+    width: 100%;
+    content: "";
+    height: 1px;
+    background: radial-gradient(
+          54.8% 53% at 50% 50%,
+          #587eb7 0%,
+          rgba(88, 126, 183, 0) 100%
+        )
+        /* warning: gradient uses a rotation that is not supported by CSS and may not behave as expected */,
+      radial-gradient(
+          60% 51.57% at 50% 50%,
+          #6d99db 0%,
+          rgba(109, 153, 219, 0) 100%
+        )
+        /* warning: gradient uses a rotation that is not supported by CSS and may not behave as expected */,
+      radial-gradient(
+          69.43% 69.43% at 50% 50%,
+          rgba(5, 5, 5, 0.5) 0%,
+          rgba(82, 82, 82, 0) 100%
+        )
+        /* warning: gradient uses a rotation that is not supported by CSS and may not behave as expected */;
+    opacity: 0.1;
+  }
+`
+
+export const Tab = styled(ClickableArea)<{ active?: boolean }>`
+  font-family: Gilroy;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 14px;
+  line-height: 100%;
+
+  text-align: center;
+  letter-spacing: 0.5px;
+
+  display: flex;
+  align-items: flex-end;
+  position: relative;
+  color: ${(props) => (props.active ? "#C5D1DC" : "#838ba3")};
+
+  &:after {
+    opacity: ${(props) => (props.active ? "1" : "0")};
+    content: "";
+    position: absolute;
+    bottom: 0;
+    right: 0;
+    left: 0;
+    margin: auto;
+    background: linear-gradient(64.44deg, #63b49b 12.29%, #a4ebd4 76.64%);
+    box-shadow: 0px 1px 4px rgba(164, 235, 212, 0.29),
+      0px 2px 5px rgba(164, 235, 212, 0.14);
+    border-radius: 2px 2px 0px 0px;
+    height: 2px;
+    width: 100%;
+  }
+`
+
+export const Title = styled(Flex)`
+  font-family: "Gilroy";
+  font-style: normal;
+  font-weight: 600;
+  font-size: 20px;
+  line-height: 32px;
+  text-align: center;
+  letter-spacing: -0.02em;
+  color: #e4f2ff;
+`
+
+export const titleVariants = {
+  visible: {
+    opacity: 1,
+    display: "flex",
+  },
+  hidden: {
+    display: "none",
+    opacity: 0,
+  },
+}
