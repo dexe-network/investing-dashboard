f4d2ec9f51c28c9158286425af1e420d52ba0b6e
Saymon <artem.simoneenko93@gmail.com>
2022-05-13 16:44:58 +0300

    Fund details page
    Styling page

--- src/pages/FundDetails/styled.tsx
+++ src/pages/FundDetails/styled.tsx
@@ -1,24 +1,25 @@
-import { Flex } from "theme"
+import { motion } from "framer-motion"
 import styled from "styled-components"
-
-export const Container = styled.div`
-  background: linear-gradient(64.44deg, #0e121b 32.35%, #0d121b 100%);
-`
 
 const variants = {
   hidden: { opacity: 0 },
   visible: { opacity: 1 },
 }
 
-export const ContentContainer = styled(Flex).attrs(() => ({
+export const Container = styled(motion.div).attrs(() => ({
   variants,
   initial: "hidden",
   animate: "visible",
   exit: "hidden",
   transition: { duration: 0.2 },
 }))`
-  width: 100%;
-  height: 100%;
-  padding: 16px;
-  flex-direction: column;
+  margin: 0 auto;
+  width: fill-available;
+  height: calc(100vh - 94px);
+  overflow-y: auto;
+  background-color: #040a0f;
+
+  @media all and (display-mode: standalone) {
+    height: calc(100vh - 115px);
+  }
 `
