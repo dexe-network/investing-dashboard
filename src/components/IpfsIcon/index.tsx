f5a9cfc554917645a61d185c7a532ae33ab4a072
Saymon <artem.simonenko@dexe.network>
2022-06-02 11:52:56 +0300

    Move IpfsIcon styles to separate file

--- src/components/IpfsIcon/index.tsx
+++ src/components/IpfsIcon/index.tsx
@@ -1,33 +1,10 @@
 import React, { useState, useEffect } from "react"
-import styled from "styled-components"
 import defaultAvatar from "assets/icons/default-avatar.svg"
 import unknown from "assets/icons/Unknown.svg"
 import { parsePoolData } from "utils/ipfs"
 import { RingSpinner } from "react-spinners-kit"
 
-export const Icon = styled.img<{ size?: number; m: string }>`
-  height: ${(props) => (props.size ? props.size : 28)}px;
-  width: ${(props) => (props.size ? props.size : 28)}px;
-  min-height: ${(props) => (props.size ? props.size : 28)}px;
-  min-width: ${(props) => (props.size ? props.size : 28)}px;
-  border-radius: 50px;
-  margin: ${(props) => props.m};
-  border: 2px solid #171b1f;
-`
-
-export const Loader = styled.div<{ size?: number; m: string }>`
-  height: ${(props) => (props.size ? props.size : 28)}px;
-  width: ${(props) => (props.size ? props.size : 28)}px;
-  min-height: ${(props) => (props.size ? props.size : 28)}px;
-  min-width: ${(props) => (props.size ? props.size : 28)}px;
-  margin: ${(props) => props.m};
-  border-radius: 50px;
-  border: 2px solid #171b1f;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  background: linear-gradient(64.44deg, #191e2b 32.35%, #272e3e 100%);
-`
+import { Icon, Loader } from "./styled"
 
 interface IProps {
   size?: number
