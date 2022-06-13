102cb7baa995b7b89567d61772ab0863d7c5e66f
Saymon <artem.simonenko@dexe.network>
2022-06-13 16:23:40 +0300

    Improved icon

--- src/assets/icons/Shrink.tsx
+++ src/assets/icons/Shrink.tsx
@@ -6,7 +6,7 @@ interface IProps extends SVGProps<SVGSVGElement> {
 
 const Shrink: FC<IProps> = ({ active = false, ...rest }) => {
   return (
-    <svg width={17} height={16} fill="none" {...rest}>
+    <svg width={16} height={16} fill="none" {...rest}>
       <path
         d="M3.833 9.333h3.334v3.333M13.167 6.666H9.833V3.333"
         stroke={active ? "#9AE2CB" : "#616D8B"}
