d9ccfcb821a45bcdb75b5088cd3c8480845a09c1
Saymon <artem.simonenko@dexe.network>
2022-06-15 19:13:08 +0300

    Provide values to FundDetailsCard

--- src/components/FundDetailsCard/index.tsx
+++ src/components/FundDetailsCard/index.tsx
@@ -1,4 +1,12 @@
-import React, { useState, useEffect } from "react"
+import { FC, useState, useEffect, useMemo } from "react"
+import { ethers } from "ethers"
+
+import { useERC20 } from "hooks/useContract"
+import { IPoolQuery, PoolInfo } from "constants/interfaces_v2"
+import { expandTimestamp, formatBigNumber } from "utils"
+import { usePoolMetadata } from "state/ipfsMetadata/hooks"
+
+import Emission from "components/Emission"
 
 import {
   Label,
@@ -7,10 +15,11 @@ import {
   InfoRow,
   EmptyDescription,
 } from "./styled"
-import { useERC20 } from "hooks/useContract"
-import { IPoolQuery, PoolInfo } from "constants/interfaces_v2"
-import { formatBigNumber } from "utils"
-import { usePoolMetadata } from "state/ipfsMetadata/hooks"
+import { format } from "date-fns"
+
+function percentage(used, total) {
+  return (used / total) * 100
+}
 
 const fundTypes = {
   BASIC_POOL: "Basic",
@@ -22,12 +31,73 @@ interface Props {
   poolInfo: PoolInfo | null
 }
 
-const FundDetailsCard: React.FC<Props> = ({ data, poolInfo }) => {
+const FundDetailsCard: FC<Props> = ({ data, poolInfo }) => {
   const [description, setDescription] = useState("")
   const [strategy, setStrategy] = useState("")
   const [, baseData] = useERC20(data.baseToken)
 
   const [{ poolMetadata }] = usePoolMetadata(data.id, data.descriptionURL)
+
+  const creationTime = useMemo(() => {
+    if (!!data) {
+      return format(expandTimestamp(data.creationTime), "MMM dd, y")
+    }
+
+    return "-"
+  }, [data])
+
+  const minimalInvestment = useMemo(() => {
+    if (!poolInfo || !baseData) return "-"
+
+    const res = ethers.utils.formatEther(poolInfo.parameters.minimalInvestment)
+
+    if (res === "0.0" || res === "0.00") {
+      return "-"
+    }
+    return `${res} ${baseData.symbol}`
+  }, [poolInfo, baseData])
+
+  const emission = useMemo(() => {
+    if (!poolInfo) return { unlimited: true, value: "Unlimited" }
+
+    const value = formatBigNumber(poolInfo.parameters.totalLPEmission, 18, 6)
+    const unlimited = value === "0.0" || value === "0.00"
+
+    return { unlimited, value: unlimited ? "Unlimited" : value }
+  }, [poolInfo])
+
+  const emissionLeft = useMemo(() => {
+    if (!poolInfo || emission.unlimited)
+      return {
+        percentage: 0,
+        value: "0.0",
+      }
+
+    const total = poolInfo.parameters.totalLPEmission
+    const used = poolInfo.lpSupply.add(poolInfo.lpLockedInProposals)
+
+    const dif = total.sub(used)
+
+    const percent = percentage(
+      Number(ethers.utils.formatEther(dif)).toFixed(2),
+      Number(ethers.utils.formatEther(total)).toFixed(2)
+    )
+
+    return {
+      percentage: percent,
+      value: formatBigNumber(dif, 18, 6),
+    }
+  }, [poolInfo, emission])
+
+  const whitelistCount = useMemo(() => {
+    if (!data) return 0
+    return data.privateInvestors.length
+  }, [data])
+
+  const adminsCount = useMemo(() => {
+    if (!data) return 0
+    return data.admins.length
+  }, [data])
 
   useEffect(() => {
     if (!poolMetadata) return
@@ -41,7 +111,7 @@ const FundDetailsCard: React.FC<Props> = ({ data, poolInfo }) => {
       <Label>Fund description</Label>
       {!description ? (
         <EmptyDescription>
-          The trader did not fill in the description of his fund.
+          Add a description of your fund for investors.
         </EmptyDescription>
       ) : (
         <DescriptionText>{description}</DescriptionText>
@@ -49,18 +119,28 @@ const FundDetailsCard: React.FC<Props> = ({ data, poolInfo }) => {
       <Label>Fund strategy</Label>
       {!strategy ? (
         <EmptyDescription>
-          The trader did not fill in the straregy of his fund.
+          Add a strategy of your fund for investors.
         </EmptyDescription>
       ) : (
         <DescriptionText>{strategy}</DescriptionText>
       )}
-      <InfoRow
-        label={"Min. investment amount"}
-        value={`0 ${baseData?.symbol}`}
-      />
+
+      {!emission.unlimited && (
+        <Emission
+          percent={emissionLeft.percentage}
+          total={`${emission.value} ${data.ticker}`}
+          current={`${emissionLeft.value} ${data.ticker}`}
+        />
+      )}
+      <InfoRow label={"Creation date"} value={creationTime} />
       <InfoRow label={"Type of fund"} value={fundTypes[data.type]} />
-      <InfoRow label={"Whitelist"} value={"0 adresess"} />
-      <InfoRow label={"Fund manager"} value={"0 managers"} />
+      <InfoRow label={"Min. investment amount"} value={minimalInvestment} />
+      <InfoRow label={"Whitelist"} value={`${whitelistCount} adresess`} />
+      <InfoRow label={"Fund manager"} value={`${adminsCount} managers`} />
+      {emission.unlimited && (
+        <InfoRow label={"Emission"} value={emission.value} />
+      )}
+
       <InfoRow
         label={"Performance Fee"}
         value={`${formatBigNumber(
