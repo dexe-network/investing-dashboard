67bcf32152fba54c8b4bdf7b49fe32d5706b0de6
edward-arinin-web-dev <edward.arinin@gmail.com>
2022-07-04 16:13:56 +0300

    add page invest to invest proposal

---
+++ src/pages/InvestInvestmentProposal/index.tsx
@@ -0,0 +1,5 @@
+const InvestInvestmentProposal = () => {
+  return <div></div>
+}
+
+export default InvestInvestmentProposal
---
+++ src/pages/InvestInvestmentProposal/useInvestInvestmentProposal.ts
@@ -0,0 +1,5 @@
+const useInvestInvestmentProposal = () => {
+  return null
+}
+
+export default useInvestInvestmentProposal
