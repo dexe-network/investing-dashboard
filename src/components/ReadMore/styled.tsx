c9dc3ba9183b3c7c487afaf0f679825a34e9caae
Saymon <artem.simonenko@dexe.network>
2022-07-05 16:46:42 +0300

    Improved ReadMore component

--- src/components/ReadMore/styled.tsx
+++ src/components/ReadMore/styled.tsx
@@ -1,5 +1,13 @@
 import styled from "styled-components"
 
-export const Link = styled.span`
-  color: #2680eb;
-`
+const Styled = {
+  Container: styled.p`
+    width: 100%;
+    cursor: pointer;
+  `,
+  Link: styled.span`
+    color: #2680eb;
+  `,
+}
+
+export default Styled
