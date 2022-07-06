dc495106ffa3a8289042076a26a406c4b20f24f4
Saymon <artem.simonenko@dexe.network>
2022-07-06 12:32:28 +0300

    Improved environment variables

--- src/hooks/useNotifications.ts
+++ src/hooks/useNotifications.ts
@@ -6,13 +6,11 @@ export default function useNotifications() {
   const [notifications, setNotifications] = useState<INotification[]>([])
 
   useEffect(() => {
-    axios
-      .get(`${process.env.REACT_APP_NOTIFICATIONS_API_URL}/notifications`)
-      .then(({ data }) => {
-        if (data.length) {
-          setNotifications(data)
-        }
-      })
+    axios.get(`/notifications`).then(({ data }) => {
+      if (data.length) {
+        setNotifications(data)
+      }
+    })
   }, [])
 
   return notifications
