e842eb95bbeb9456ae462ebfc180bae0b780c987
Saymon <artem.simonenko@dexe.network>
2022-07-08 10:57:44 +0300

    Improved investor positions UI

--- src/pages/InvestmentPositions/styled.tsx
+++ src/pages/InvestmentPositions/styled.tsx
@@ -5,13 +5,13 @@ const Styled = {
   List: styled.div`
     width: 100%;
     height: fill-content;
-    height: calc(100vh - 128px);
+    height: calc(100vh - 174px);
     padding: 16px;
     position: relative;
     overflow-y: auto;
 
     @media all and (display-mode: standalone) {
-      height: calc(100vh - 149px);
+      height: calc(100vh - 197px);
     }
   `,
   Content: styled(Flex)`
